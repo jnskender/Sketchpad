546866ce20cd838b89c18f4bfe001d797ba5727e
John Skender <jnskender@gmail.com>
2017-02-06 00:23:38 -0600

    Changin grid size now works

--- js/script.js
+++ js/script.js
@@ -3,44 +3,23 @@ $(document).ready(function() {
     var default_grid_size = 16;
     var size = default_grid_size;
 
+
     addGrid(size);
-    $("#btn_size_sbmt").click(function() {
-        var user_size_string = $("#grid_size_text").val();
-        var user_size_int = parseInt($("#grid_size_text").val());
-        if (isNaN(user_size_int)) {
-            alert(user_size_string + " is not a valid number! Try Again!");
-            console.log(user_size_int);
-        }else if(user_size_string.indexOf(' ') >= 0){
-          alert("Value cannot contain spaces");
-        } else {
-            clearGrid();
-            size = user_size_int;
-            addGrid(size);
-            draw("black");
-        }
-    });
     draw("black");
+    $("#btn_size_sbmt").click(function() {
+        $(".grid").remove();
+        changeSize();
+    });
 }); //End Document Ready
 var block_size;
 
-/*
 function addGrid(size) {
+  var node = "";
     for (i = 1; i <= size * size; i++) {
-        $('<div></div>').addClass("grid").css({
-            "width": setSize(size),
-            "height": setSize(size)
-        }).appendTo(".sketchpad");
-    }
-}
-*/
-
-var node = "";
-function addGrid(size) {
-    for (i = 1; i <= size * size; i++) {
-     node += "<div class='grid' style='width:" + setSize(size) + 'px;' + "height:" + setSize(size) + "px';></div>";
+        node += "<div class='grid' style='width:" + setSize(size) +
+            'px;' + "height:" + setSize(size) + "px';></div>";
     }
     $(".sketchpad").append(node);
-    console.log(node);
 }
 
 
@@ -56,6 +35,17 @@ function draw(color) {
     });
 }
 
-function clearGrid() {
-    $(".sketchpad").empty();
+function changeSize() {
+    var user_size_string = prompt("Enter Grid Size: ");
+    var user_size_int = parseInt(user_size_string);
+    if (isNaN(user_size_int)) {
+        alert(user_size_string + " is not a valid number! Try Again!");
+        console.log(user_size_int);
+    } else if (user_size_string.indexOf(' ') >= 0) {
+        alert("Value cannot contain spaces");
+    } else {
+        size = user_size_int;
+        addGrid(size);
+        draw("black");
+    }
 }
